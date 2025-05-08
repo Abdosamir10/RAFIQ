ab64c37fd511bbd835fb5b4a311d5d5d9c38f2e3
Abdosamir10 <155080502+Abdosamir10@users.noreply.github.com>
2025-05-08 23:31:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const text = "مرحباً بك في مجتمع رفيق الأكثر أماناً";
+// let i = 0;
+// const speed = 10; // السرعة بين الحروف (بالمللي ثانية)
+
+// function typeEffect() {
+//   if (i < text.length) {
+//     document.getElementById("typing-text").innerHTML += text.charAt(i);
+//     i++;
+//     setTimeout(typeEffect, speed);
+//   }
+// }
+
+// window.onload = typeEffect;
+
+
+
+
